ac4cf42b422ce6e911b358d51b6b1186615f5b28
he.yongdong <heyd@derlook.com>
2021-07-29 10:09:06 +0800

    添加运行㊗主文件

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == '__main__':
     logging.info("============================开始============================")
-    py_file = 'streamlit run %s/tool.py' % os.path.dirname(__file__)
+    py_file = 'streamlit run %s/streamlit_app.py' % os.path.dirname(__file__)
     p = os.system(py_file)
